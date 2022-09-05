74a02cb166dc0b621c852b8b28fb70974ed48eeb
63070028 <63070028@kmitl.ac.th>
2022-09-05 22:12:14 +0700

    Final Full
    Final Full

--- src/main/java/com/example/week4/Cashier.java
+++ src/main/java/com/example/week4/Cashier.java
@@ -10,13 +10,15 @@ public class Cashier {
     @RequestMapping(value = "/getChange/{n}")
     public Change getChange(@PathVariable("n") int num){
         Change c = new Change();
-        c.setB1000(num/1000);
-        c.setB500(num%1000/500);
-        c.setB100((num%1000)%500/100);
-        c.setB20((((num%1000)%500)%100)/20);
-        c.setB10(((((num%1000)%500)%100)%20)/10);
-        c.setB5((((((num%1000)%500)%100)%20)%10)/5);
-        c.setB1((((((num%1000)%500)%100)%20)%10)%5);
+        if(num >= 0){
+            c.setB1000(num/1000);
+            c.setB500(num%1000/500);
+            c.setB100((num%1000)%500/100);
+            c.setB20((((num%1000)%500)%100)/20);
+            c.setB10(((((num%1000)%500)%100)%20)/10);
+            c.setB5((((((num%1000)%500)%100)%20)%10)/5);
+            c.setB1((((((num%1000)%500)%100)%20)%10)%5);
+        }
         return c;
     }
 }
